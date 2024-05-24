13661d9b701f449d081c88da5ea85caa17cc1779
Paulo Sergio <pauloserg861@gmail.com>
2024-05-24 18:49:02 -0300

    feat: config view code setup

--- Weather App/ViewController.swift
+++ Weather App/ViewController.swift
@@ -8,12 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // utilizando a lazy var o componente será criado quando uma referencia de lazy var for criada
+    private lazy var customView: UIView = {
+        let view = UIView(frame: .zero)
+        // pode usar UIColor.black ou somente o .
+        view.backgroundColor = .black
+        // ao criar uma constraint programaticamente é necessario setar a
+        // propriedade a baixo como false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
+        // Só é executado uma vez quando a tela é carregada
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .red
+        
+        
+        setHierarchy()
+        setConstraints()
+    }
+    
+    // metodo para setar a hierarquia da view
+    private func setHierarchy() {
+        view.addSubview(customView)
+    }
+    
+    private func setConstraints() {
+        // se ficar negativa fazer analogia com o plano carteziano
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
+    }
 }
 
